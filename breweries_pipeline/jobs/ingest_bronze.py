c42b723346b48a4b05ba57ee5dd733322782a611
pedro <pedrohv_henrique@hotmail.com>
2026-02-19 21:30:55 -0300

    ingest_bronze job initial setup + contract validations

--- breweries_pipeline/jobs/ingest_bronze.py
+++ breweries_pipeline/jobs/ingest_bronze.py
@@ -1,1 +1,40 @@
-# 
+from airflow.exceptions import AirflowFailException
+import logging
+
+from breweries_pipeline.apis.client import get_all_breweries
+from breweries_pipeline.datalake.filesystem import save_to_bronze
+from breweries_pipeline.quality.contract import validate_breweries_data
+
+
+logger = logging.getLogger(__name__)
+
+def ingest_to_bronze():
+    # fetch the breweries data form the API
+    breweries = get_all_breweries()
+
+    # get data contract validation results
+    is_valid, errors = validate_breweries_data(breweries)
+
+    # logs if validation failed
+    if not is_valid:
+        logger.warning("Data contract validation failed.")
+
+    # checks for critical errors that stop the pipeline execution. Hard fail, can't proceed with invalid data type
+    if errors["data_type"]:
+        raise AirflowFailException("Invalid data type returned by API (expected list of breweries).")
+
+    # hard fail in this case since we're fetching all time data
+    elif errors["data_empty"]:
+        raise AirflowFailException("No data returned by API, expected a list of breweries.")
+
+    # checks for non critical errors that allow the pipeline to proceed
+    missing = errors.get("missing_fields_indexes", [])
+    if missing:
+        logger.warning("Some records are missing expected fields at indexes: %s", missing) # logs errors found
+        breweries = [b for idx, b in enumerate(breweries) if idx not in set(missing)] # remove invalid records
+
+        if not breweries: # if no records left, hard fails
+            raise AirflowFailException("All records are missing expected fields, no valid data to save to bronze layer.")
+
+    # saves data to bronze layer
+    save_to_bronze(breweries, "breweries.json")
--- breweries_pipeline/quality/contract.py
+++ breweries_pipeline/quality/contract.py
@@ -3,3 +3,37 @@
 # Validates data against expectations
 #
 # Returns validation results and error details, if any
+
+expected_brewery_fields = [
+    "id",
+    "name",
+    "brewery_type",
+    "city",
+    "country",
+    "state",
+]
+
+def validate_breweries_data(data):
+    errors = {
+        "data_type": False,
+        "data_empty": False,
+        "missing_fields_indexes": []
+    }
+
+    if not isinstance(data, list):
+        errors["data_type"] = True
+        # early return, can't proceed with further checks on invalid data type
+        return False, errors
+
+    if len(data) == 0:
+        errors["data_empty"] = True
+        # early return, can't check if there are no records
+        return False, errors
+    
+    for idx, brewery in enumerate(data):
+        for field in expected_brewery_fields:
+            if field not in brewery:
+                errors["missing_fields_indexes"].append(idx)
+                break # no need to check other fields if one is missing
+    
+    return True, errors
