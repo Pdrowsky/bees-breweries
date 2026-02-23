bcdbaca7b33e9ccade8c1501da2a588e43b64ca5
pedro <pedrohv_henrique@hotmail.com>
2026-02-23 14:23:13 -0300

    docker and airflow setup files + breweries_dag.py + requirements.txt

--- breweries_pipeline/config.py
+++ breweries_pipeline/config.py
@@ -3,5 +3,4 @@
 import os
 from pathlib import Path
 
-#TODO: set it up on docker-compose
 LAKE_ROOT = os.getenv("LAKE_ROOT", str(Path.cwd() / "storage"))
--- dags/breweries_dag.py
+++ dags/breweries_dag.py
@@ -1,2 +1,34 @@
-# dag for the pipeline
+from airflow.decorators import dag, task
+from datetime import datetime
 
+from breweries_pipeline.jobs.ingest_bronze import ingest_to_bronze
+from breweries_pipeline.jobs.transform_silver import transform_silver
+from breweries_pipeline.jobs.build_gold import build_gold
+
+@dag(
+    dag_id="breweries",
+    start_date=datetime(2025, 1, 1),
+    schedule=None,
+    catchup=False,
+)
+def breweries():
+
+    @task
+    def bronze_task(**context) -> str:
+        run_id = context["run_id"]
+        ds = context["ds"] # YYYY-MM-DD
+        return ingest_to_bronze(run_id=run_id, execution_time=ds)
+
+    @task
+    def silver_task(bronze_path: str) -> str:
+        return transform_silver(bronze_path=bronze_path)
+
+    @task
+    def gold_task(silver_path: str) -> str:
+        return build_gold(silver_path=silver_path)
+
+    bronze_path = bronze_task()
+    silver_path = silver_task(bronze_path)
+    gold_task(silver_path)
+
+breweries()
