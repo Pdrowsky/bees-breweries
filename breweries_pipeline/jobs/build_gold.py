7475b1b34b4f6499354292faf772eca118ebfcaa
pedro <pedrohv_henrique@hotmail.com>
2026-02-22 22:05:52 -0300

    build_gold.py built with aggregated view + small change on transform_silver.py to return filepath and drop extra unused columns

--- breweries_pipeline/jobs/build_gold.py
+++ breweries_pipeline/jobs/build_gold.py
@@ -1,1 +1,22 @@
-# 
+from breweries_pipeline.datalake.filesystem import read_from_silver, save_to_gold
+
+
+def build_gold(silver_path: str):
+    """builds an aggregated view of the quantity of breweries by country and state,
+    then saves it to the gold layer as a parquet file"""
+
+    # reads data from silver layer
+    breweries_df = read_from_silver(silver_path)
+
+    # aggregate the quantity of breweries by country and state
+    agg_breweries = breweries_df.groupby(
+        ["brewery_type","country", "state"], observed=True
+        ).size().reset_index(name="brewery_count")
+
+    # save the aggregated data to gold layer
+    filename = "breweries/aggregated/breweries_by_type_country_state.parquet"
+    save_to_gold(agg_breweries, filename)
+
+    # create aggregated view with 
+
+    return filename
--- breweries_pipeline/jobs/transform_silver.py
+++ breweries_pipeline/jobs/transform_silver.py
@@ -14,11 +14,14 @@ def transform_silver(bronze_path: str):
     breweries_df = json_to_dataframe(breweries)
 
     # drop unnecessary columns to optimize storage and performance
-    keep = ["id", "name", "brewery_type", "country", "state", "city", "latitude", "longitude"]
+    keep = ["id", "name", "brewery_type", "country", "state", "city"]
     breweries_df = breweries_df[keep]
 
     # remove duplicates based on id
     breweries_df = breweries_df.drop_duplicates(subset="id")
+
+    # drop rows with missing country or state values (ensure partitioning columns are not null)
+    breweries_df = breweries_df.dropna(subset=["country", "state"])
 
     # split dataframe based on country column
     breweries_by_country = split_df_by_column(breweries_df, "country")
@@ -35,6 +38,11 @@ def transform_silver(bronze_path: str):
     # save each dataframe to the silver layer, partitioned by country and state
     for country, states in breweries_by_country.items():
         for state, df in states.items():
-            filename = f"breweries/country={clean_string(country)}/state={clean_string(state)}/data.parquet"
-            save_to_silver(df, filename)
 
+            df = df.drop(columns=["country", "state"])
+
+            # clean country and state strings to ensure valid file paths
+            filename = f"breweries/country={clean_string(country)}/state={clean_string(state)}/data.parquet"
+            filepath = save_to_silver(df, filename)
+
+    return filepath.split('/country')[0]
